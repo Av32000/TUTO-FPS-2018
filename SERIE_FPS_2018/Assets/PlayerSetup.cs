18b4244f7d6118f60e730a75a665166e0438245c
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-02-21 15:51:25 +0100

    Épisode 3
    Setup de UNET.

---
+++ SERIE_FPS_2018/Assets/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            // boucle pour désactiver les components des autres joueurs sur notre instance
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
