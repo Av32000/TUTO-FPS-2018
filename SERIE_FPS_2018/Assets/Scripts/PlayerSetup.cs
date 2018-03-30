0f253e4095d7e553d9a95ed660a2cc285c3ccb29
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-03-30 10:43:00 +0200

    Episode 6

---
+++ SERIE_FPS_2018/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    private string remoteLayerName = "RemotePlayer";
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+
+        RegisterPlayer();
+    }
+
+    private void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    private void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    private void DisableComponents()
+    {
+        // boucle pour désactiver les components des autres joueurs sur notre instance
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ SERIE_FPS_2018/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour {
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+	void Start () {
+		if(cam == null)
+        {
+            Debug.LogError("Pas de caméra référencée.");
+            this.enabled = false;
+        }
+	}
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    private void Shoot()
+    {
+        RaycastHit _hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        {
+            if(_hit.collider.tag == "Player")
+            {
+                CmdPlayerShot(_hit.collider.name);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot(string _ID)
+    {
+        Debug.Log(_ID + " a été touché.");
+    }
+
+}
